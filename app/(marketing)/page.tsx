6cb9e108911cdf3c84cc7e4407b78c74ba446797
hesham1010 <hesham.gublootech@gmail.com>
2024-01-29 21:06:02 +0500

    CHANGES

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button"
 import { Medal } from "lucide-react"
-
+import Link from "next/link"
+import localFont from 'next/font/local'
+import { cn } from "@/lib/utils"
+const headingFont = localFont({
+    src: "../../public/fonts/font.woff2"
+})
 const MarketingPage = () => {
     return (
         <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center flex-col">
+            <div className={cn("flex items-center justify-center flex-col", headingFont.className)}>
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className=" h-6 w-6 mr-2" />
                     No 1 task management
@@ -13,9 +19,10 @@ const MarketingPage = () => {
                     work forward.
                 </div>
             </div>
-            <div>
-                Collaborate, manage projects, and reach new productivity peaks. From
+            <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl mx-auto">
+                Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify
             </div>
+            <Button className="mt-6" size="lg" > <Link href={'/sign-up'}>Get Taskify for free</Link></Button>
         </div>
     )
 }
